4164e32db538d0924443a218119ac82af50679e7
skoyamamd <skoyama@broadinstitute.org>
2021-01-30 16:50:41 +0000

    first

---
+++ src/gtex.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/broadinstitute/gtex-pipeline/master/rnaseq/Dockerfile -O ../dockerfile/gtex_v9.dockerfile
+spython recipe ../dockerfile/gtex_v9.dockerfile > ../recipe/Singularity.v9
+
+
